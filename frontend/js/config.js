ee891d27d68906c9417d87c0ed7914123f0d174e
AyaHemdan <yoyohem603@gmail.com>
2025-11-19 16:13:34 +0200

    Update API URL for production

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,5 @@
 const API_CONFIG = {
-    BASE_URL: 'https://learn-cybersecurity-backend.vercel.app',
+    BASE_URL: 'http://localhost:5000',
     ENDPOINTS: {
         SIGNUP: '/api/auth/register',
         SIGNIN: '/api/auth/login'
